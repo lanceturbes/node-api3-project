201523006a6c8f6230e4bb07c53662f9b9359ae4
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-27 13:31:26 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+// Imports
+const express = require("express");
 
+// Declare server
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+// Use middleware
+server.use(express.json());
 
-// global middlewares and the user's router need to be connected here
-
-server.get('/', (req, res) => {
+// Handle requests
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// Exports
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// require your server and launch it
+// Imports
+const server = require("./api/server");
+
+// Start server
+const port = 5000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
