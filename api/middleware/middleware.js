16bed020b0d1ed5a34df84e6d71b40e515f81bf0
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-27 14:24:02 -0700

    created validateUserId middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,22 @@
+const Post = require("./../posts/posts-model");
+const User = require("./../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const targetUser = await User.getById(id);
+    if (targetUser) {
+      next();
+    } else {
+      next({ status: 404, message: `user with id ${id} not found!` });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +28,6 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  validateUserId,
+};
